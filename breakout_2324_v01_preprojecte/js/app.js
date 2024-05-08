3245476bc6fa8a49293e5522f35263c49534221b
Davidrieca <157466156+Davidrieca@users.noreply.github.com>
2024-05-08 18:02:49 +0200

    MENU top
    millores

--- breakout_2324_v01_preprojecte/js/app.js
+++ breakout_2324_v01_preprojecte/js/app.js
@@ -18,3 +18,25 @@ function animacio() {
     requestAnimationFrame(animacio);    
 }
 
+function activarHover1(boton) { 
+    removerHover();
+    boton.classList.add("hover-activado1");
+}
+
+function activarHover2(boton) {
+    removerHover();
+    boton.classList.add("hover-activado2");
+
+}
+
+function activarHover3(boton) {
+    removerHover();
+    boton.classList.add("hover-activado3");
+}
+
+function removerHover() {
+    var botones = document.querySelectorAll(".buttoneasy, .buttonmedium, .buttondifi");
+    botones.forEach(function (boton) {
+        boton.classList.remove("hover-activado1", "hover-activado2", "hover-activado3", "hover-activado4");
+    });
+}
