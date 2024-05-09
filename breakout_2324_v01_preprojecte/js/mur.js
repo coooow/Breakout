777c1770d7e8b9a56ff6896d7a02bd741838990a
uni-roro <rrodap@edu.tecnocampus.cat>
2024-05-09 09:18:13 +0200

    update mur

--- breakout_2324_v01_preprojecte/js/mur.js
+++ breakout_2324_v01_preprojecte/js/mur.js
@@ -5,27 +5,39 @@
 class Mur {
     constructor() {
         defineixNivells();
-        let totxos = []
+        let totxosArray = [];
     }
 
+    /*
+    Això genera els valors d'una array de totxos bassant-se en una String que prové de 
+    defineixNivells() si hi ha una 'a' s'afegeix un totxo, sinó simplement avança la
+    posició fins trobar una altre 'a' o fins que s'acabi la String
+    */
     generaMur(lvl) {
-        let totxosArray = [];
-
+        totxosArray = [];       //buida la Array per si es necessités tornar a usar el mètode
         for (let i = 0; i < this.nivells[lvl].totxos.length; i++) {
             for (let j = 0; j < this.nivells[lvl].totxos[i].length; j++) {
+
                 const puntPosicio = { x: i, y: j };
-                const amplada = 20;
+                const amplada = 20;     //← i ↓ es poden cambiar segons gust
                 const alcada = 5;
+                
                 const character = this.nivells[lvl].totxos[i][j];
                 if (character == 'a') {
-                    const totxo = new Totxo(puntPosicio, amplada, alcada);
+                    const totxo = new totxo(puntPosicio, amplada, alcada);
                     totxosArray.push(totxo);
                 }
             }
         }
     }
+
+    /*
+    Segurament malament però ioquese
+    */
     draw(ctx) {
-        //for (int i; i < nivells[])
+        for (let i = 0; i < totxosArray.length; i++){
+            totxosArray[i].draw(ctx);
+        }
     }
 
     defineixNivells() {
