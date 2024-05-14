0e23b61cc5c3204b8392b0b7596956773fe41188
Davidrieca <157466156+Davidrieca@users.noreply.github.com>
2024-05-14 09:56:31 +0200

    Menu casi finish
    dalkmjdpskahnfpajij

--- breakout_2324_v01_preprojecte/js/app.js
+++ breakout_2324_v01_preprojecte/js/app.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     joc = new Joc(myCanvas,ctx);
     joc.inicialitza();
     animacio();
+    mostrarRanking();
 
 });
 
@@ -38,5 +39,35 @@ function removerHover() {
     var botones = document.querySelectorAll(".buttoneasy, .buttonmedium, .buttondifi");
     botones.forEach(function (boton) {
         boton.classList.remove("hover-activado1", "hover-activado2", "hover-activado3", "hover-activado4");
+    });
+}
+
+function guardarYMostrarRanking() {
+    const playerName = document.getElementById('player-name').value;
+    if (playerName.trim() !== "") {
+        guardarJugador(playerName);
+        mostrarRanking();
+        document.getElementById('player-name').value = '';
+    }
+}
+
+function guardarJugador(nombre) {
+    if (!nombre) return;
+
+    let jugadores = JSON.parse(localStorage.getItem('jugadores')) || [];
+    jugadores.push({ nombre: nombre, puntuacion: 0 }); 
+    jugadores = jugadores.slice(0, 5);
+    localStorage.setItem('jugadores', JSON.stringify(jugadores));
+}
+
+function mostrarRanking() {
+    const rankingList = document.getElementById('ranking-list');
+    rankingList.innerHTML = '';
+
+    const jugadores = JSON.parse(localStorage.getItem('jugadores')) || [];
+    jugadores.forEach(jugador => {
+        const li = document.createElement('li');
+        li.textContent = jugador.nombre;
+        rankingList.appendChild(li);
     });
 }
