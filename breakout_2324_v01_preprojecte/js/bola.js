e908e4811dceadd5f743686aec7b30d69615c43c
cow <marxfox@gmail.com>
2024-05-22 08:59:36 +0200

    xoc mejor

--- breakout_2324_v01_preprojecte/js/bola.js
+++ breakout_2324_v01_preprojecte/js/bola.js
@@ -88,38 +88,25 @@ class Bola {
             if (this.interseccioSegmentRectangle(trajectoria, joc.totxosArray[i])) {
                 switch (this.interseccioSegmentRectangle(trajectoria, joc.totxosArray[i]).vora) {
                     case "superior":
-                        exces = (trajectoria.puntB.y - this.radi) / this.vy;
-                        this.posicio.y = this.posicio.y - this.radi;
-                        xoc = true;
                         this.vy = -this.vy;
                         break;
                     case "esquerra":
-                        this.posicio.x = this.posicio.x + this.radi;
                         this.vx = -this.vx;
-                        xoc = true;
                         break;
                     case "dreta":
-                        this.posicio.x = this.posicio.x - this.radi;
                         this.vx = -this.vx;
-                        xoc = true;
                         break;
                     case "inferior":
-                        this.posicio.y = this.posicio.y + this.radi;
                         this.vy = -this.vy;
-                        xoc = true;
                         break;
                 }
+                xoc = true;
                 toxtoXoc = true;
                 joc.totxosArray[i].tocat = true;
                 joc.totxosArray.splice(i, 1);
-<<<<<<< Updated upstream
                 this.vx += 0.05;
                 this.vy += 0.05;
                 joc.punts += 10;
-=======
-  
-                this.punts += 10;
->>>>>>> Stashed changes
             }
 
             i++;
